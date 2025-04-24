6fb8035a3930710f57b147c05e51475b93752b83
shaheen-kubihal <kspatange630@gmail.com>
2025-04-24 11:05:01 +0530

    save file

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@ public static void main(String[] args) {
     int a=20; int b=5;
     System.out.println("Addiytion : "+(a+b));
     System.out.println("Substraction :"+(a-b));
-    System.out.println("multiplication :"+(a*b));
-    System.out.println("division  : "+(a/b));
+  
+
 }
 }
